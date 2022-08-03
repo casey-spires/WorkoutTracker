730ee4b0598c4548845191ebf51d63ce7d267815
Casey D Spires <11319139+casey-spires@users.noreply.github.com>
2022-08-02 22:13:27 -0400

    Adds generic repository

---
+++ src/WorkoutTracker.Application/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkoutTracker.Core.Interfaces;
+using WorkoutTracker.Infrastructure.Data;
+
+namespace WorkoutTracker.Application.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly WorkoutTrackerContext _context;
+        public GenericRepository(WorkoutTrackerContext context) {
+            _context = context;
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+        public void RemoveAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+    }
+}
---
+++ src/WorkoutTracker.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetByIdAsync(int id);
+        IEnumerable<T> GetAllAsync();
+        IEnumerable<T> FindAsync(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+        void AddAsync(T entity);
+        void Remove(T entity);
+
+    }
+}
