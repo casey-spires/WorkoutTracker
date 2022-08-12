a7a97b2706e48ecec8d0f736c0b5143efdf409bb
Casey D Spires <11319139+casey-spires@users.noreply.github.com>
2022-08-11 21:26:56 -0400

    Adds DbContext

--- src/WorkoutTracker.API/Program.cs
+++ src/WorkoutTracker.API/Program.cs
@@ -1,3 +1,5 @@
+using WorkoutTracker.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<WorkoutTrackerContext>(options =>
+{
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- src/WorkoutTracker.Infrastructure/Data/WorkoutTrackerContext.cs
+++ src/WorkoutTracker.Infrastructure/Data/WorkoutTrackerContext.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WorkoutTracker.Core
+using WorkoutTracker.Core;
 using WorkoutTracker.Core.Entities;
 
 namespace WorkoutTracker.Infrastructure.Data
