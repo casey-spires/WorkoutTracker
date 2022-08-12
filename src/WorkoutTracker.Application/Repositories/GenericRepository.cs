162fda4432ac579f972645b3cf0a0f283cdd626a
Casey D Spires <11319139+casey-spires@users.noreply.github.com>
2022-08-11 21:27:10 -0400

    Adds generic repository

--- src/WorkoutTracker.Application/Repositories/GenericRepository.cs
+++ src/WorkoutTracker.Application/Repositories/GenericRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using WorkoutTracker.Core.Interfaces;
@@ -15,14 +16,10 @@ namespace WorkoutTracker.Application.Repositories
         public GenericRepository(WorkoutTrackerContext context) {
             _context = context;
         }
-        public async Task<T> GetByIdAsync(int id)
-        {
-            return await _context.Set<T>().FindAsync(id);
-        }
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            return await _context.Set<T>().ToListAsync();
-        }
+        public async Task<T?> GetByIdAsync(int id) => await _context.Set<T>().FindAsync(id);
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+     
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression) => await _context.Set<T>().Where(expression).ToListAsync();
         public async Task AddAsync(T entity)
         {
             await _context.Set<T>().AddAsync(entity);
@@ -31,7 +28,7 @@ namespace WorkoutTracker.Application.Repositories
         {
             _context.Set<T>().Update(entity);
         }
-        public void RemoveAsync(T entity)
+        public void Remove(T entity)
         {
             _context.Set<T>().Remove(entity);
         }
--- src/WorkoutTracker.Core/Interfaces/IGenericRepository.cs
+++ src/WorkoutTracker.Core/Interfaces/IGenericRepository.cs
@@ -9,11 +9,11 @@ namespace WorkoutTracker.Core.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
-        T GetByIdAsync(int id);
-        IEnumerable<T> GetAllAsync();
-        IEnumerable<T> FindAsync(Expression<Func<T, bool>> expression);
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression);
         void Update(T entity);
-        void AddAsync(T entity);
+        Task AddAsync(T entity);
         void Remove(T entity);
 
     }
